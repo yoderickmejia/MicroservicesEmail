dfee40396b47c2cd02add5ef65b3c7fda7411b34
Yoderick Montero <ymontero@gruporead.com>
2025-04-10 15:22:15 -0400

    today

--- comments/index.js
+++ comments/index.js
@@ -1,30 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');  
-const {randomBytes} = require('crypto');    
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios"); 
 
 const commentsByPostId = {};
 
 const app = express();
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(cors()); // Enable CORS for all routes
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+   await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  })
+  
+  res.status(201).send(comments);
+});
 
+app.post('/events', (req, res) => {
+  console.log('Event received:', req.body.type);
+  res.send({}); // Acknowledge the event
+}
+);
 
-app.post('/posts/:id/comments',  (req, res) => {      
-  const commentId = randomBytes(4).toString('hex');  
-  const {content} = req.body;                       
-  const comments = commentsByPostId[req.params.id] || [];  
-  comments.push({id: commentId, content});               
-  commentsByPostId[req.params.id] = comments;            
-  res.status(201).send(comments);                        
-}   );  
-
-
-app.listen(4001, () => {    
-    console.log('Listening on 4001');
-    }   );      
+app.listen(4001, () => {
+  console.log("Listening on 4001");
+});
