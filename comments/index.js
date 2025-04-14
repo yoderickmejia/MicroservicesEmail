29fbdab85162ae2a4352ac7a00d868d0285ca941
Yoderick Montero <ymontero@gruporead.com>
2025-04-14 12:07:06 -0400

    kLK

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content , status: "pending" });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -37,29 +37,31 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Event Received", req.body.type);
+app.post("/events", async (req, res) => {
+  console.log("Event Received:", req.body.type);
 
   const { type, data } = req.body;
+
   if (type === "CommentModerated") {
-    const { id, postId, status } = data;
+    const { postId, id, status, content } = data;
     const comments = commentsByPostId[postId];
+
     const comment = comments.find((comment) => {
       return comment.id === id;
     });
-
     comment.status = status;
 
-    axios.post("http://localhost:4005/events", {
+    await axios.post("http://localhost:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
         status,
         postId,
-        content: comment.content,
+        content,
       },
     });
   }
+
   res.send({});
 });
 
--- query/index.js
+++ query/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,24 +8,36 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post("/events", (req, res) => {
+app.post('/events', (req, res) => {
   const { type, data } = req.body;
 
-  if (type === "PostCreated") {
+  if (type === 'PostCreated') {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === "CommentCreated") {
+  if (type === 'CommentCreated') {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
     post.comments.push({ id, content, status });
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
   }
 
   console.log(posts);
@@ -34,5 +46,5 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4002, () => {
-  console.log("Listening on 4002");
+  console.log('Listening on 4002');
 });
