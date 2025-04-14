383b15b6879474fa6bfdacc2d9f53ae2c021d767
Yoderick Montero <ymontero@gruporead.com>
2025-04-14 11:56:20 -0400

    moderation

--- comments/index.js
+++ comments/index.js
@@ -40,6 +40,26 @@ app.post("/posts/:id/comments", async (req, res) => {
 app.post("/events", (req, res) => {
   console.log("Event Received", req.body.type);
 
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { id, postId, status } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content: comment.content,
+      },
+    });
+  }
   res.send({});
 });
 
