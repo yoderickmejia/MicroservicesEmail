7bf587a02cc859d8854b9db2aa00477b8a702a56
Yoderick Montero <ymontero@gruporead.com>
2025-04-14 12:06:33 -0400

    algo

--- moderation/index.js
+++ moderation/index.js
@@ -1,32 +1,30 @@
-const express   = require("express");
-const bodyParser = require("body-parser");
-const axios     = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
-
-const app = express();  
+const app = express();
 app.use(bodyParser.json());
 
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
 
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-app.post("/events", async (req, res) => {
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
 
-    const { type, data } = req.body;    
-    if (type === "CommentModerated") {
-      const status = data.content.includes("approved") ? "approved" : "rejected";  
-        await axios.post("http://localhost:4005/events", {
-            type: "CommentUpdated",
-            data: {
-                id: data.id,
-                postId: data.postId,
-                status,
-                content: data.content,
-            },
-        });
-    }
-    res.send({" status": "OK" });   
-})
-
+  res.send({});
+});
 
 app.listen(4003, () => {
-    console.log("Listening on 4003")
-})
+  console.log('Listening on 4003');
+});
