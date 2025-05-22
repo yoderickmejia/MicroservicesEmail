e06f752812537d126c0d2f262dd5f4742fc73d91
Yoderick Montero <ymontero@gruporead.com>
2025-05-22 11:59:49 -0400

    dockerizando services

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,16 +8,16 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post("http://moderation-srv:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
+  axios.post("http://query-srv:4003/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
